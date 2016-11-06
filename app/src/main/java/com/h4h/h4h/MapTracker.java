54e1367481365d3b9a3fb654c94b176aa2a31c84
glenn222 <g2ngo@uwaterloo.ca>
2016-11-06 00:17:50 -0400

    Retrieve a user's current location

--- app/src/main/java/com/h4h/h4h/MapTracker.java
+++ app/src/main/java/com/h4h/h4h/MapTracker.java
@@ -1,7 +1,14 @@
 package com.h4h.h4h;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +20,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapTracker extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final int REQ_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +47,53 @@ public class MapTracker extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
 
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        if (checkPermission()) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            Toast.makeText(this, "You have to accept to enjoy all app's services!", Toast.LENGTH_LONG).show();
+            askPermission();
+        }
+
+    }
+
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+    // Asks for permission
+    private void askPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                REQ_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    if (checkPermission())
+                        mMap.setMyLocationEnabled(true);
+
+                } else {
+                    // Permission denied
+                    Toast.makeText(this, "Permission to access location denied", Toast.LENGTH_SHORT);
+                }
+                break;
+            }
+        }
     }
 }
